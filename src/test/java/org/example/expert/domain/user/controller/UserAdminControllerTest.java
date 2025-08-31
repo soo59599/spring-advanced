4b23d262337e67e6eb9fd604c7a14eb106be2698
soo59599 <sooh59599@gmail.com>
2025-09-01 01:07:45 +0900

    레벨 7 : User - Controller 테스트 코드 작성

---
+++ src/test/java/org/example/expert/domain/user/controller/UserAdminControllerTest.java
@@ -0,0 +1,50 @@
+package org.example.expert.domain.user.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.example.expert.common.interceptor.AuthInterceptor;
+import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
+import org.example.expert.domain.user.service.UserAdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(UserAdminController.class)
+public class UserAdminControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UserAdminService userAdminService;
+
+	@MockBean
+	private AuthInterceptor authInterceptor;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	void User의_Role을_바꿀_수_있다() throws Exception{
+		//given
+		when(authInterceptor.preHandle(any(), any(), any())).thenReturn(true);
+		long userId = 1L;
+		UserRoleChangeRequest userRoleChangeRequest = new UserRoleChangeRequest("ADMIN");
+		String requestBody = objectMapper.writeValueAsString(userRoleChangeRequest);
+
+		//when //then
+		mockMvc.perform(patch("/admin/users/{userId}", userId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(requestBody))
+			.andExpect(status().isOk());
+
+	}
+}
